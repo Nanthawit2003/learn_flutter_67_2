c52e515e20ef7ac26bcb12fff9923849131ea9b7
Nanthawit <sadzasad747@gmail.com>
2025-06-14 11:01:42 +0700

    Step 3: stateless widget

--- lib/main.dart
+++ lib/main.dart
@@ -38,13 +38,42 @@ void main() {
 }
 
 // Step 2: stateless widget
-class Home  extends StatelessWidget {
-  const Home ({super.key});
+// class Home  extends StatelessWidget {
+//   const Home ({super.key});
 
-  @override
+//   @override
+//   Widget build(BuildContext context) {
+//     return Text("Hello Flutter");
+//   }
+// }  
+
+//  Step 3: stateless widget
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override 
   Widget build(BuildContext context) {
-    return Text("Hello Flutter");
-  }
-}  
+   // return Container(color: Colsor.blueAccent, width: 300, height: 300);
+   // return Container(color: Colors.blueAccent, margin: EdgeInsets.all(10));
+  //  return Container(
+  //   color: Colors.blueAccent,
+  //   margin: EdgeInsets.fromLTRB(5, 10, 15, 20),
+  //  );
+    return Center(
+      child:Container(
+        color:Colors.blueAccent,
+        margin: EdgeInsets.symmetric(horizontal: 20, vertical: 30),
+        padding: EdgeInsets.all(20),
+        child: Text(
+          "Hello Flutter",
+          style: TextStyle(fontSize: 30, letterSpacing: 3),
+        ),
+      ),
+    );
+  } 
+}
+
+
+
 
 
