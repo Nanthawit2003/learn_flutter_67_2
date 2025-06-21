5782f492b2b51696db78b6a613206f69f554bdfa
Nanthawit <sadzasad747@gmail.com>
2025-06-21 10:21:35 +0700

    step 9: listview widget

--- lib/screen/item.dart
+++ lib/screen/item.dart
@@ -23,28 +23,54 @@ class _ItemState extends State<Item> {
     });
   }
 
+// step 9: Listview widget
+List data = ["Isak", "Wirtz", "Kerkaze", "Frimpong", "Mamardashvili"];
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            "Quantity: $quantity", style: const TextStyle(fontSize: 24)),
-          const SizedBox(height: 20),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              ElevatedButton(onPressed: addQuantity, child: const Text("Add")),
-              SizedBox(width: 20),
-              ElevatedButton(
-                onPressed: subtractQuantity,
-                child:  Text("Subtract"),
-              ),
-            ],
+    // return Center(
+    //   child: Column(
+    //     mainAxisAlignment: MainAxisAlignment.center,
+    //     children: [
+    //       Text(
+    //         "Quantity: $quantity", style: const TextStyle(fontSize: 24)),
+    //       const SizedBox(height: 20),
+    //       Row(
+    //         mainAxisAlignment: MainAxisAlignment.center,
+    //         children: [
+    //           ElevatedButton(onPressed: addQuantity, child: const Text("Add")),
+    //           SizedBox(width: 20),
+    //           ElevatedButton(
+    //             onPressed: subtractQuantity,
+    //             child:  Text("Subtract"),
+    //           ),
+    //         ],
+    //       ),
+    //     ],
+    //   ),
+    // );
+  
+    // step 9: listview widget
+    return ListView.builder(
+      itemBuilder: (context, index) {
+      return Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(30),
+          color: Colors.lightGreenAccent,
+        ),
+        margin: EdgeInsets.symmetric(vertical: 2, horizontal: 5),
+        padding: EdgeInsets.all(40),
+        child: Text(
+          data[index],
+          style: TextStyle(
+            fontSize: 24, 
+            color: Colors.white, 
+            fontWeight: FontWeight.bold,
           ),
-        ],
-      ),
+        ),
+      );
+    },
+    itemCount: data.length,
     );
+   }
   }
-}
